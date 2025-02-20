aea1bda621dd63fc707a542943f9db1e87c31c8e
abhishek <chauhanabhi.mail@gmail.com>
2025-02-20 23:09:02 +0530

    main screen tabs added

---
+++ app/src/main/java/com/example/tudu/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.tudu
+
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.tudu.screens.MainScreen
+import com.example.tudu.ui.theme.TuduTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            installSplashScreen()
+        }
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TuduTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    containerColor = Color.White
+                ) { innerPadding ->
+                    App(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun App(modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = "main") {
+        composable("main") { MainScreen(modifier) }
+    }
+}
---
+++ app/src/main/java/com/example/tudu/screens/MainScreen.kt
@@ -0,0 +1,108 @@
+package com.example.tudu.screens
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /* Handle click */ },
+                containerColor = Color.Black,
+                shape = CircleShape
+            ) {
+                Icon(
+                    Icons.Default.Add, contentDescription = "Add Todo",
+                    tint = Color.White
+                )
+            }
+        }
+    ) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(12.dp, 0.dp)
+        ) {
+            Header()
+        }
+    }
+
+}
+
+@Composable
+fun Header(modifier: Modifier = Modifier) {
+
+    val buttonEnabled = remember { mutableIntStateOf(1) }
+    val btnText = listOf("All", "Today", "Upcoming", "Overdue")
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start,
+        modifier = Modifier
+            .fillMaxWidth(1f)
+    ) {
+        Text(
+            style = MaterialTheme.typography.headlineMedium,
+            text = "Task List",
+            fontWeight = FontWeight.SemiBold
+        )
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth(1f)
+        ) {
+
+            (1..4).forEach { index ->
+                Button(
+                    {
+                        buttonEnabled.intValue = index
+                    },
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = if (buttonEnabled.intValue == index) Color.White else Color.Black,
+                        containerColor = if (buttonEnabled.intValue == index) Color.Black else Color(
+                            0xFFDDE0E5
+                        )
+                    ),
+                    contentPadding = PaddingValues(16.dp, 8.dp)
+                ) {
+                    Text(text = btnText[index - 1], style = MaterialTheme.typography.titleSmall)
+                }
+            }
+
+        }
+    }
+}
